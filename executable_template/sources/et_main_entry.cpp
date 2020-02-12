acca39d73822ccbbf18b09353b8eafbf02715f50
jiteman <jiteman@gmail.com>
2020-02-12 15:47:50 +0300

    C/C++ settings have been separated

--- executable_template/sources/et_main_entry.cpp
+++ executable_template/sources/et_main_entry.cpp
@@ -1,5 +1,8 @@
 #include "et/et_class.h"
 
+#include "et/et_function.h"
+#include "dlt/dlt_function.h"
+#include "blt/blt_function.h"
 
 #include <iostream>
 
@@ -10,6 +13,10 @@ int main( int const /*argc*/, char const *const * /*argv*/ ) {
 	::std::cout << example->say_base_hello() << ::std::endl;
 	::std::cout << example->say_derived_hello() << ::std::endl;
 	::std::cout << example->say_executable_hello() << ::std::endl;
+
+	::std::cout << jmsd_blt_base_function_say_function_hello() << ::std::endl;
+	::std::cout << jmsd_dlt_derived_function_say_executable_hello() << ::std::endl;
+	::std::cout << jmsd_et_executable_function_say_executable_hello() << ::std::endl;
 
 	delete example;
 
